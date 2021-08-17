489658c768a850045973d68d3d3b257f90d60cfb
= <=>
2021-08-17 20:17:23 -0300

    adding glue trigger job and simulate data to test workflow

--- infrastructure/glue-job.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_glue_job" "etl" {
-  name     = "EducalakeETL"
-  role_arn = aws_iam_role.glue_role.arn
-
-  command {
-    script_location = "s3://${aws_s3_bucket.codes.bucket}/etl.py"
-  }
-}
