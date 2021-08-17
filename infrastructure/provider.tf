eacfc417512a24801cf8fa1876b954951cdabb86
= <=>
2021-08-17 01:00:09 -0300

    adding state control

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+terraform {
+  backend "s3" {
+      bucket = "educalake-iac-state"
+      key = "terraform.state"
+      region = "us-east-2"
+  }
+}
