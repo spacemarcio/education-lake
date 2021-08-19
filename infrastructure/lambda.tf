1bebfa354c9afe973d2892a6d4b461f7033b5393
= <=>
2021-08-18 23:04:50 -0300

    fixing lambda iac file

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,12 +1,12 @@
 resource "aws_lambda_function" "crawling_data" {
-  filename      = "lambda_deploy.zip"
+  filename      = "lambda_function_payload.zip"
   function_name = "EducalakeCrawler"
   role          = aws_iam_role.lambda.arn
   handler       = "lambda_function.handler"
   memory_size   = 128
   timeout       = 30
 
-  source_code_hash = filebase64sha256("../lambda/lambda_deploy.zip")
+  source_code_hash = filebase64sha256("../lambda/lambda_function_payload.zip")
 
   runtime = "python3.8"
 }
