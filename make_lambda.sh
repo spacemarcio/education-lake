4ea1e658b7c597f51027ea7940003985ca0b10a0
= <=>
2021-08-18 23:00:33 -0300

    fixing make lambda script

--- make_lambda.sh
+++ make_lambda.sh
@@ -2,6 +2,6 @@ cd lambda
 mkdir package
 pip install --target ./package -r lambda_requirements.txt
 cd package
-zip -r ../deploy-package.zip .
+zip -r ../lambda_function_payload.zip .
 cd ..
 zip -g lambda_function_payload.zip lambda_function.py
