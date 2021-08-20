b83621e842f210cf17d1de33867c5d7183c6f8bb
= <=>
2021-08-20 10:12:52 -0300

    fixing lambda

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,13 +1,17 @@
 import boto3
 import requests
-import pandas as pd 
+import csv
 
 url = 's3://inep-censo-files/escolas.csv'
 
 def handler(event, context):
     
-    data = pd.read_csv(url,delimiter='|')
-    data.to_csv('/tmp/data.csv',sep = '\t', index = False)
+    response = requests.get(url)        
+
+    with open('/tmp/data.csv', 'w') as f:
+        writer = csv.writer(f)
+        for line in response.iter_lines():
+            writer.writerow(line.decode('utf-8').split(','))
     
     s3_client = boto3.client('s3')
     s3_client.upload_file('/tmp/data.csv', 'educalake-raw', 'DATA.csv')
