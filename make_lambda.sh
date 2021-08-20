9beadf4f4fad07a98861b9015a4a26ff10cfc933
= <=>
2021-08-20 09:41:16 -0300

    change lambda deploy

--- make_lambda.sh
+++
@@ -1,7 +0,0 @@
-cd lambda
-mkdir package
-pip install --target ./package -r lambda_requirements.txt
-cd package
-zip -r ../lambda_function_payload.zip .
-cd ..
-zip -g lambda_function_payload.zip lambda_function.py
