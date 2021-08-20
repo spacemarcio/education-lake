dcfdb73f9862031f4402d01cc52ae185896caf2d
= <=>
2021-08-20 13:22:08 -0300

    adding glue job

--- infrastructure/glue.tf
+++ infrastructure/glue.tf
@@ -1,20 +1,20 @@
-# resource "aws_glue_job" "etl_job" {
-#   name     = "EducalakeETL"
-#   role_arn = aws_iam_role.glue_role.arn
+resource "aws_glue_job" "etl_job" {
+  name     = "EducalakeETL"
+  role_arn = aws_iam_role.glue_role.arn
 
-#   command {
-#     script_location = "s3://${aws_s3_bucket.codes.bucket}/etl.py"
-#   }
-# }
+  command {
+    script_location = "s3://${aws_s3_bucket.codes.bucket}/etl.py"
+  }
+}
 
-# resource "aws_glue_trigger" "etl_trigger" {
-#   name = "EducalakeTrigger"
-#   type = "ON_DEMAND"
+resource "aws_glue_trigger" "etl_trigger" {
+  name = "EducalakeTrigger"
+  type = "ON_DEMAND"
 
-#   actions {
-#     job_name = aws_glue_job.etl_job.name
-#   }
-# }
+  actions {
+    job_name = aws_glue_job.etl_job.name
+  }
+}
 
 resource "aws_glue_catalog_database" "staging_zone" {
   name = "staging_db"
